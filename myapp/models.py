d8c0a81df37afb835731a8686aae909c7ca10c20
gk-svg <gk32239@gmail.com>
2020-07-04 23:11:44 +0530

    course model created

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+class Course(models.Model):
+    name=models.CharField(max_length=200)
+    category=models.CharField(max_length=50)
+    rate=models.IntegerField(default=5,validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ])
+    price=models.CharField(max_length=5)
