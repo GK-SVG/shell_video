f68d3979564e908055f22a61a2b7235fcbef9d7a
gk-svg <gk32239@gmail.com>
2020-07-08 00:17:06 +0530

    models registered to admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import (
-    CourseCategary)
+    CourseCategary,
+    CourseLesson)
 # Register your models here.
 admin.site.register(CourseCategary)
+admin.site.register(CourseLesson)
