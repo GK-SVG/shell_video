d403551c69586264a8b3038612566c9a1b5df459
gk-svg <gk32239@gmail.com>
2020-07-11 02:17:08 +0530

    validations link data model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class StoreMailVarificatioLink(models.Model):
+    token=models.TextField()
+    email=models.CharField(max_length=150)
+    uid=models.IntegerField()
+
+    class Meta:
+        db_table='store_user_mail_valification_link'
+    def __str__(self):
+        return 'user id ' + str(self.uid)
+    
