d0ead1356b923ac2526670c1b8ef8dc7428e48df
gk-svg <gk32239@gmail.com>
2020-07-11 00:58:48 +0530

    token generater added

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib import messages
 from .models import Users
-
+from services.views import send_verification_link
 
 
 def login(request):
@@ -14,34 +14,31 @@ def signup(request):
         email=request.POST['email']
         pass1=request.POST['pass1']
         pass2=request.POST['pass2']
-        print(username,pass1,email)
         # checkpoints 
         #username length checker
-        if len(username) > 12 :
-            messages.error(request,'Username must have maximum 12 Charcters')
-            return redirect('/')
+        
 
         # username charkters checker
         if not username.isalnum() :
              messages.error(request,'Username only contain alphaNumeric value')
-             return redirect('/')       
+             return HttpResponse('Username only contain alphaNumeric value')       
 
         # password1 and password2 checker     
         if pass1 != pass2 :
             messages.error(request,'Passwords do not match')
-            return redirect('/')
+            return HttpResponse('Passwords do not match')
         # creating user
         try:
-            myuser=Users.objects.get(username=username)
+            myuser=Users.objects.get(user=username)
             messages.error(request,'The username you entered has already been taken. Please try another username.')
-            return redirect('/')
+            return HttpResponse('The username you entered has already been taken. Please try another username')
         except:
-            myuser = Users(username=username,email=email,password=pass1,phone=phone)
+            myuser = Users(user=username,email=email,password=pass1,phone=phone)
             myuser.save()
-            messages.success(request,'Your account created succesfully')
-            return redirect('/')      
+            token=send_verification_link(myuser)
+            return HttpResponse('Account created successfully token === \n'+ str(token))      
     else:
-        return redirect('/')
+        return redirect('')
 
 def logout(request):
     return HttpResponse('this is logout')
