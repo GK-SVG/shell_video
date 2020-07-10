142f3f6824e438b5b1dedd61452f2c566f8ad56c
gk-svg <gk32239@gmail.com>
2020-07-11 00:57:04 +0530

    user detatils renamed for token generater

--- user/models.py
+++ user/models.py
@@ -3,11 +3,11 @@ from django.utils.timezone import now
 
 # Create your models here
 class Users(models.Model):
-    username=models.CharField(max_length=50)
+    user=models.CharField(max_length=50)
     email=models.CharField(max_length=100)
     password=models.CharField(max_length=16)
     phone=models.CharField(max_length=15)
-    created_date=models.DateTimeField(auto_now_add=True)
+    last_login=models.DateTimeField(auto_now_add=True)
     class Meta:
         db_table='users'
     def __str__(self):
